bbda2964af3ab8e4ed860c49181e71978895ebe9
Nick Webb <nickw@redwireservices.com>
2015-06-12 17:20:47 -0700

    Simplify to just do chef_gem invocation, nothing else, zilch.

--- attributes/default.rb
+++
@@ -1,7 +0,0 @@
-# phpunit
-default['phpunit']['checksum'] = 'f44b9ede13261bc742d8cf6453771ef2ee7d94bf12ea2bcc9bad69266bb6ffad'
-default['phpunit']['path'] = '/usr/local/bin'
-default['phpunit']['basename'] = 'phpunit-'
-default['phpunit']['version'] = '4.6.9'
-default['phpunit']['extensionname'] = '.phar'
-default[:tile_web][:s3packagebucket] = 'packages.rwsnw.com'
--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,10 @@
 name 'tabula-rasa-test'
 description 'Test to reproduce issue #6'
 long_description 'xxx'
-license 'Proprietary - All Rights Reserved'
+license 'Apache2.0'
 version "1.0.0"
 supports "amazon"
-recipe "tile-web::default", "Configure apache for cgis"
-provides "tile-web::default"
+recipe "tabula-rasa-test::breaks", "break Tabula Rasa with chef_gem"
+provides "tabula-rasa-test::breaks"
 maintainer 'Nick Webb'
 maintainer_email 'nickw@redwireservices.com'
-depends "s3_file", "~> 2.5.1"
