ea1878a15aa72e8e1bc6cf5d12ea3dd11d269d6a
Nick Webb <nickw@redwireservices.com>
2015-06-16 09:20:00 -0700

    Grab file with s3_file to exercise/test rest-client install.

--- recipes/breaks.rb
+++ recipes/breaks.rb
@@ -1,3 +1,13 @@
 chef_gem "rest-client" do
   action :install
 end
+
+# Grab custom postgres package & install
+s3_file "/tmp/tabula-rasa-issue-6-test.tar.gz" do
+  remote_path "/tabula-rasa-issue-6-test.tar.gz"
+  bucket "shlomo-test"
+  owner "root"
+  group "root"
+  mode "0644"
+  not_if "test -f /tmp/tabula-rasa-issue-6-test.tar.gz"
+end
