e77f38aed6b1a0156dec8277e72bb44fa9ba24cb
Nick Webb <nickw@redwireservices.com>
2015-06-16 10:06:01 -0700

    Update package to download to something we control.

--- recipes/breaks.rb
+++ recipes/breaks.rb
@@ -3,10 +3,10 @@ chef_gem "rest-client" do
 end
 
 # Grab custom postgres package & install
-s3_file "/tmp/tabula-rasa-issue-6-test.tar.gz" do
-  remote_path "/tabula-rasa-issue-6-test.tar.gz"
-  bucket "shlomo-test"
-  s3_url "https://shlomo-test.s3.amazonaws.com"
+s3_file "/tmp/phpunit-4.6.9.phar" do
+  remote_path "/phpunit-4.6.9.phar"
+  bucket "packages.rwsnw.com"
+  s3_url "https://s3-us-west-2.amazonaws.com/packages.rwsnw.com"
   owner "root"
   group "root"
   mode "0644"
